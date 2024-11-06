5a94ccfadaecc9eb9035d2bb1daa5701095a8d7a
Kei-K23 <keiksl2301@gmail.com>
2024-11-06 20:21:51 +0630

    Add update and delete route handler for role

--- controllers/role_controller.go
+++ controllers/role_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/Kei-K23/user-management-system-api/dto"
 	"github.com/Kei-K23/user-management-system-api/services"
 	"github.com/gofiber/fiber/v2"
@@ -28,4 +30,50 @@ func (r *RoleController) CreateRole(c *fiber.Ctx) error {
 	}
 
 	return c.Status(fiber.StatusCreated).JSON(role)
+}
+
+func (r *RoleController) GetRoleById(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error when converting string to int"})
+	}
+
+	role, err := r.roleService.GetById(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error when getting role"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(role)
+}
+
+func (r *RoleController) GetRoleByName(c *fiber.Ctx) error {
+	name := c.Query("name")
+
+	role, err := r.roleService.GetByName(name)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error when getting role"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(role)
+}
+
+func (r *RoleController) UpdateRole(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error when converting string to int"})
+	}
+
+	var input dto.UpdateRoleInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+	}
+
+	role, err := r.roleService.Update(id, input.Name, input.Description)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error when updating role"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(role)
 }
---
+++ dto/update_role_input.go
@@ -0,0 +1,6 @@
+package dto
+
+type UpdateRoleInput struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
