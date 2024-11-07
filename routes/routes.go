71fd6e85fda063bdc228815c4dc6ce265dc23d51
Kei-K23 <keiksl2301@gmail.com>
2024-11-07 15:54:58 +0630

    Add user routes

--- routes/routes.go
+++ routes/routes.go
@@ -19,4 +19,14 @@ func SetupRoutes(app *fiber.App) {
 	apiV1.Post("/roles", roleController.CreateRole)
 	apiV1.Put("/roles/:id", roleController.UpdateRole)
 	apiV1.Delete("/roles/:id", roleController.DeleteRole)
+
+	// User repository setup
+	userRepo := repositories.NewRoleRepository()
+	userService := services.NewRoleService(userRepo)
+	userController := controllers.NewRoleController(*userService)
+	apiV1.Get("/users", userController.GetRoles)
+	apiV1.Get("/users/:id", userController.GetRoleById)
+	apiV1.Post("/users", userController.CreateRole)
+	apiV1.Put("/users/:id", userController.UpdateRole)
+	apiV1.Delete("/users/:id", userController.DeleteRole)
 }
