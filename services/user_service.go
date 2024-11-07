ce15786f938f533405d3baad20e9a9815e66fac7
Kei-K23 <keiksl2301@gmail.com>
2024-11-07 15:41:45 +0630

    Add user CRUD service

--- services/user_service.go
+++ services/user_service.go
@@ -14,11 +14,11 @@ func NewUserService(userRepo repositories.UserRepository) *UserService {
 	return &UserService{userRepo}
 }
 
-func (r *UserService) Create(username, fullName, email, password string, roleId int) error {
+func (u *UserService) Create(username, fullName, email, password string, roleId int) (*models.User, error) {
 
 	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	user := &models.User{
@@ -29,5 +29,39 @@ func (r *UserService) Create(username, fullName, email, password string, roleId 
 		RoleId:   roleId,
 	}
 
-	return r.userRepo.CreateUser(user)
+	return u.userRepo.CreateUser(user)
+}
+
+func (u *UserService) Update(id int, username, fullName, email, password string, roleId int) (*models.User, error) {
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &models.User{
+		Username: username,
+		FullName: fullName,
+		Email:    email,
+		Password: string(hashedPass),
+		RoleId:   roleId,
+	}
+
+	return u.userRepo.UpdateUser(id, user)
+}
+
+func (u *UserService) Delete(id int) (int, error) {
+	return u.userRepo.DeleteUser(id)
+}
+
+func (u *UserService) GetUsers() ([]*models.User, error) {
+	return u.userRepo.GetUsers()
+}
+
+func (u *UserService) GetById(id int) (*models.User, error) {
+	return u.userRepo.GetUserById(id)
+}
+
+func (u *UserService) GetByUsername(name string) (*models.User, error) {
+	return u.userRepo.GetUserByUsername(name)
 }
